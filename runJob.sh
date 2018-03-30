59a75459f9c56ad17cd761b99a21e3c9dc675467
luicasko <l.kramarik@hotmail.sk>
2018-03-30 13:00:02 +0200

    jobs

--- runJob.sh
+++ runJob.sh
@@ -4,7 +4,7 @@ productionId=`date +%F_%H-%M`
 echo ${productionId}
 mkdir -p workDir/${productionId}
 cd workDir/${productionId}
-
+echo "Start of cps."
 cp -Lr /gpfs01/star/pwg/lkramarik/sim/fastsimD0_2016/submit/* ./
 cp ../../toyMcEffZeroDecayLength.C ./
 cp ../../Momentum_resolution_SL16d.root ./
@@ -17,12 +17,14 @@ cp ../../Eff_PionPlus_embedding.root ./
 cp ../../Eff_PionMinus_embedding.root ./
 cp ../../Eff_KaonPlus_embedding.root ./
 cp ../../Eff_KaonMinus_embedding.root ./
+echo "Copying done."
 
 rm -r LocalLibraries.package
 rm LocalLibraries.zip
 
-set prodId=`date +%F_%H-%M`
 codePath="/gpfs01/star/pwg/lkramarik/sim/fastsimD0_2016/workDir/"${productionId}
+echo codePath
+
 mkdir ./jobs
 mkdir ./out/
 mkdir ./myOutput/
@@ -32,4 +34,6 @@ mkdir ./jobs/report/
 mkdir ./jobs/list
 mkdir ./jobs/csh
 
-star-submit-template -template submitToyMcZeroVtx.xml -entities productionId=${prodId},codePath=$codePath
+echo "Directories created. Let's submit some jobs..."
+
+star-submit-template -template submitToyMcZeroVtx.xml -entities productionId=${productionId},codePath=$codePath
