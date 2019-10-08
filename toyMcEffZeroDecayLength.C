1e75d6c57657bfbf1eece0a2207f32e4ac34a1fe
luicasko <l.kramarik@hotmail.sk>
2019-10-08 17:33:20 +0200

    added pid

--- toyMcEffZeroDecayLength.C
+++ toyMcEffZeroDecayLength.C
@@ -692,6 +692,7 @@ bool matchHft(int const iParticleIndex, double const vz, int const zdcb, TLorent
    if (mom.Perp()>12) return false;
    if (mom.Perp()<0.15) return false;
    int const bin = hHftRatio1[iParticleIndex][iEtaIndex][iVzIndex][iPhiIndex][zdcb]->FindBin(mom.Perp());
+   cout<<"bin ok"<<endl;
    if (bin<1) return false;
    return gRandom->Rndm() < hHftRatio1[iParticleIndex][iEtaIndex][iVzIndex][iPhiIndex][zdcb]->GetBinContent(bin);
 }
