e750f8a63388307cec974f37458dc21d4dd4c826
lukas <l.kramarik@hotmail.sk>
2020-03-02 20:27:15 +0100

    refmult implemented

--- toyMcEffZeroDecayLength.C
+++ toyMcEffZeroDecayLength.C
@@ -586,7 +586,7 @@ TVector3 smearPosData(int const iParticleIndex, double const vz, int zdcb, TLore
     double sigmaPosZ = 0;
     double sigmaPosXY = 0;
 
-    h2Dca[iParticleIndex][iEtaIndex][iVzIndex][zdcbinDCA][iPtIndex][centrality]->GetRandom2(sigmaPosXY,sigmaPosZ);
+    h2Dca[iParticleIndex][iEtaIndex][iVzIndex][iPtIndex][centrality]->GetRandom2(sigmaPosXY,sigmaPosZ);
     sigmaPosZ *= 1.e4;
     sigmaPosXY *= 1.e4;
 
