81ace9a060a5a902b112e4f8f9300bef9d23e5bd
lukas <l.kramarik@hotmail.sk>
2020-03-03 13:43:10 +0100

    refmult implemented

--- toyMcEffZeroDecayLength.C
+++ toyMcEffZeroDecayLength.C
@@ -241,7 +241,7 @@ void decayAndFill(int const kf, TLorentzVector* b, double const weight, TClonesA
 void fill(int const kf, TLorentzVector* b, double weight, TLorentzVector const& kMom, TLorentzVector const& pMom, TVector3 v00)
 {
     cout<<"Fill() start"<<endl;
-    double refMult = hRefMult->GetRandom();
+    Double_t refMult = hRefMult->GetRandom();
     cout<<refMult<<endl;
     int centrality = getMultiplicityBin(refMult);
     cout<<refMult<<" "<<centrality<<endl;
