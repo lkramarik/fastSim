e0f94793ddffa71e2c817e3f2d29014c429e5f9f
lukas <l.kramarik@hotmail.sk>
2020-08-13 17:14:01 +0200

    new nonprimary

--- toyMcEffZeroDecayLength_hijingInputs.C
+++ toyMcEffZeroDecayLength_hijingInputs.C
@@ -845,8 +845,7 @@ void bookObjects()
     //getting VZ histogram for each multiplicity
    cout<<"Loading Vz and ZDCs..."<<endl;
     TFile fEvent("inputs.event_hijing.root");
-    TH3F* mh3VzZdcMult = new TH3F();
-    mh3VzZdcMult = (TH3F*)fEvent.Get("mh3VzZdcMult");
+    TH3F* mh3VzZdcMult = (TH3F*)fEvent.Get("mh3VzZdcMult");
 //    hRefMult = (TH1D*)fEvent.Get("hrefMult");
 
     TFile fVertexReso("vertexReso.root");
